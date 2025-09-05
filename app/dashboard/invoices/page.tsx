72ae48962d7fdb20c071d984c7bde6b12de21136
CCCCOOH <c3156298376@163.com>
2025-09-06 04:25:11 +0800

    完成流式传输部分的学习

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,11 @@
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { CardSkeleton, LatestInvoicesSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+
 export default function Page() {
-  return (<p>Invoices Page</p>)
+  return (
+    <Suspense fallback={<LatestInvoicesSkeleton />}>
+      <LatestInvoices />
+    </Suspense>
+  )
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -11,6 +11,7 @@ import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
@@ -18,10 +19,10 @@ export async function fetchRevenue() {
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-
+    await new Promise((resolve) => setTimeout(resolve, 3000))
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
